fe9586207f2ee084db136969ccff3cdb36199814
darktend <vladgodunqwer@gmail.com>
2021-03-12 21:20:26 +0200

    i done task bracets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ for(let i=0;i<bracketsConfig.length;){
+let strOff=bracketsConfig[i].join("");
+if(str.includes(strOff)){
+  str=str.replace(strOff,"");
+  i=0
+}
+else{
+  i++
+}
+ }
+ return str.length==0?true:false
 }
